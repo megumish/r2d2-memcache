6cac2b48f426d3e30198a3f746d9a8a06b82338d
An Long <aisk1988@gmail.com>
2018-12-21 12:47:09 +0800

    fix get_thousand_data_parallel test

--- tests/test_numerous_connection.rs
+++ tests/test_numerous_connection.rs
@@ -16,25 +16,39 @@ fn get_thousand_data_parallel() {
         let mut conn = pool.get().unwrap();
         conn.flush().unwrap();
     }
+
+    pool.get().unwrap().flush().unwrap();
+
+    let mut threads = vec![];
     for i in 0..1000 {
         let pool = pool.clone();
-        thread::spawn(move || {
+        let t = thread::spawn(move || {
             let mut conn = pool.get().unwrap();
-            conn.flush().unwrap();
-            let value = format!{"{}{}","bar_thousand",i};
-            conn.set(&format!("{}{}", "foo_thousand", i), value, 0)
+            let value = format!{"{}{}","bar_thousand_",i};
+            conn.set(&format!("{}{}", "foo_thousand_", i), value, 0)
                 .unwrap();
         });
+        threads.push(t);
     }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    let mut threads = vec![];
     for i in 0..1000 {
         let pool = pool.clone();
-        thread::spawn(move || {
-            let value = format!{"{}{}","bar_thousand",i};
+        let t = thread::spawn(move || {
+            let value = format! {"{}{}","bar_thousand_",i};
             let mut conn = pool.get().unwrap();
-            let result: String = conn.get(&format!("{}{}", "foo_thousand", i))
+            let result: String = conn
+                .get(&format!("{}{}", "foo_thousand_", i))
                 .unwrap()
                 .unwrap();
             assert!(result == value);
         });
+        threads.push(t);
+    }
+    for thread in threads {
+        thread.join().unwrap();
     }
 }
