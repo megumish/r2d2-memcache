1c7de3cfdfaaa74e15cec20f4d631e9de641fc89
An Long <aisk1988@gmail.com>
2019-01-05 00:06:50 +0800

    update document and bump version

--- target/doc/implementors/r2d2/trait.ManageConnection.js
+++ target/doc/implementors/r2d2/trait.ManageConnection.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["r2d2_memcache"] = [{text:"impl <a class=\"trait\" href=\"r2d2/trait.ManageConnection.html\" title=\"trait r2d2::ManageConnection\">ManageConnection</a> for <a class=\"struct\" href=\"r2d2_memcache/connection_manager/struct.MemcacheConnectionManager.html\" title=\"struct r2d2_memcache::connection_manager::MemcacheConnectionManager\">MemcacheConnectionManager</a>&lt;'static&gt;",synthetic:false,types:["r2d2_memcache::connection_manager::MemcacheConnectionManager"]},];
+implementors["r2d2_memcache"] = [{text:"impl <a class=\"trait\" href=\"r2d2/trait.ManageConnection.html\" title=\"trait r2d2::ManageConnection\">ManageConnection</a> for <a class=\"struct\" href=\"r2d2_memcache/connection_manager/struct.MemcacheConnectionManager.html\" title=\"struct r2d2_memcache::connection_manager::MemcacheConnectionManager\">MemcacheConnectionManager</a>",synthetic:false,types:["r2d2_memcache::connection_manager::MemcacheConnectionManager"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
